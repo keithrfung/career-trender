c821dbeeb400b17a6bee45f512e5c24bb96273f5
Keith Fung <keith.robert.fung@gmail.com>
2016-02-21 21:20:10 -0500

    Fix issue with inability to view homepage

--- Models/GitJobSearchResult.cs
+++ Models/GitJobSearchResult.cs
@@ -5,5 +5,6 @@ namespace career_trender.Models
        public string Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public GitJobSearchResult(){}
     }
 }
